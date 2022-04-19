5f1927acc9e7cd2624d1e47f7bf2c73e8fd3fae0
Pedro Henrique Alves <phphalves@gmail.com>
2022-04-19 20:25:00 -0300

    Criando getters e setters por lombook. Adicionando entidade

--- src/main/java/com/santander/cardspending/domain/Launch.java
+++ src/main/java/com/santander/cardspending/domain/Launch.java
@@ -1,11 +1,27 @@
 package com.santander.cardspending.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
 public class Launch implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
@@ -14,79 +30,6 @@ public class Launch implements Serializable {
     private String tags;
 
     public Launch(){
-
-    }
-
-    public Launch(Integer id, String name, String description, Timestamp register, Double value, String tags) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.register = register;
-        this.value = value;
-        this.tags = tags;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Timestamp getRegister() {
-        return register;
-    }
-
-    public void setRegister(Timestamp register) {
-        this.register = register;
-    }
-
-    public Double getValue() {
-        return value;
-    }
-
-    public void setValue(Double value) {
-        this.value = value;
-    }
-
-    public String getTags() {
-        return tags;
-    }
-
-    public void setTags(String tags) {
-        this.tags = tags;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Launch launch = (Launch) o;
-
-        return id != null ? id.equals(launch.id) : launch.id == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
     }
 
 }
