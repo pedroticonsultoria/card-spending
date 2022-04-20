abecc82c83451f7f0083776c5341acee3b842a57
Pedro Henrique Alves <phphalves@gmail.com>
2022-04-20 12:13:04 -0300

    Alterando tipo de ID

--- src/main/java/com/santander/cardspending/repositories/LaunchRepository.java
+++ src/main/java/com/santander/cardspending/repositories/LaunchRepository.java
@@ -4,6 +4,8 @@ import com.santander.cardspending.domain.Launch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface LaunchRepository extends JpaRepository <Launch,Integer> {
+public interface LaunchRepository extends JpaRepository <Launch, UUID> {
 }
