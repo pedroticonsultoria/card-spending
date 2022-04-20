61948986a66f56731acd5fcf08a6fb852660f52d
Pedro Henrique Alves <phphalves@gmail.com>
2022-04-20 12:14:10 -0300

    Alterando tipo ID e adicionando rest get all.

--- src/main/java/com/santander/cardspending/resource/LaunchResource.java
+++ src/main/java/com/santander/cardspending/resource/LaunchResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 
 @RestController
@@ -19,17 +20,22 @@ public class LaunchResource {
     private LaunchService service;
 
     @RequestMapping(value="/{id}",method=RequestMethod.GET)
-    public ResponseEntity<?> find(@PathVariable Integer id){
+    public ResponseEntity<?> find(@PathVariable UUID id){
 
         Launch object = service.find(id);
         return ResponseEntity.ok().body(object);
     }
 
+    @RequestMapping(method=RequestMethod.GET)
+    public ResponseEntity<?> find(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
     @RequestMapping(method=RequestMethod.POST)
-    public ResponseEntity<Void> insert(@RequestBody Launch object){
+    public ResponseEntity<Launch> insert(@RequestBody Launch object){
         object = service.insert(object);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(object.getId()).toUri();
-        return ResponseEntity.created(uri).build();
+        return ResponseEntity.created(uri).body(object);
     }
 }
