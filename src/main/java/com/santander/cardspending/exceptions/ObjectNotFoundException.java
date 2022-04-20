8549fcc26c420850e86e09453c9395a6a50ad5aa
Pedro Henrique Alves <phphalves@gmail.com>
2022-04-20 11:12:59 -0300

    Tratamento de objeto não encontrado específico

---
+++ src/main/java/com/santander/cardspending/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.santander.cardspending.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String mensage){
+        super(mensage);
+    }
+
+    public ObjectNotFoundException(String mensage, Throwable cause){
+        super(mensage,cause);
+    }
+}
---
+++ src/main/java/com/santander/cardspending/resource/exception/StandardError.java
@@ -0,0 +1,19 @@
+package com.santander.cardspending.resource.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    private Integer status;
+    private String mensage;
+    private Long timeStamp;
+}
